b5d90536d15c1b68aa89593b446f746a375d6775
Tushar Gaurav <tushar.gaurav416@gmail.com>
2026-01-29 23:46:06 +0530

    fix: update s3 config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -58,6 +58,7 @@ export default buildConfig({
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
         },
         region: process.env.S3_REGION,
+        endpoint: process.env.S3_ENDPOINT,
       },
     }),
   ],
